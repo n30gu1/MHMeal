5fe056b6f87615152be5619f484277c643e18def
n30gu1 <n30gu1@icloud.com>
2019-12-15 18:45:31 +0900

    Initial Commit

---
+++ Geupsik/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Geupsik
+//
+//  Created by 박성헌 on 2019/12/15.
+//  Copyright © 2019 n30gu1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
