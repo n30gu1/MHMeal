a509e657d9abe2014bd550b2bbb209d00868f1d4
Seongheon Park <n30gu1@n30gu1.com>
2021-07-11 22:52:15 +0900

    Refactoring - changing layouts

---
+++ Geupsik/DateSelBoxView.swift
@@ -0,0 +1,21 @@
+//
+//  DateSelBoxView.swift
+//  Geupsik
+//
+//  Created by 박성헌 on 2021/07/11.
+//  Copyright © 2021 n30gu1. All rights reserved.
+//
+
+import SwiftUI
+
+struct DateSelBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateSelBoxView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateSelBoxView()
+    }
+}
